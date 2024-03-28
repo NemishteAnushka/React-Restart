3f23421b1ea06931276fcdb44a535a569de8647a
NemishteAnushka <nemishte.anushka23@gmail.com>
2024-03-28 22:39:28 +0530

    map method

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,43 @@
 import React from "react";
-import Props from "./components/props/Props";
+import RenderList from "./components/listRender/RenderList";
+// import ChildrenProps from "./components/props/ChildrenProps";
+// import Props from "./components/props/Props";
 // import HelloWorld from "./components/HelloWorld";
 // import Greetings from "./components/Greetings";
-
+const users = [
+  { userId: 1, firstName: "Anushka", lastName: "Nemishte" },
+  { userId: 2, firstName: "Anuja", lastName: "Nemishte" },
+  { userId: 3, firstName: "Neha", lastName: "Kulkarni" },
+  { userId: 4, firstName: "Prajkata", lastName: "Patil" },
+];
 function App() {
-  const arrayProps = ["fruit", "vegetable"];
-  const objectProps = { hobby: "dance" };
+  // const arrayProps = ["fruit", "vegetable"];
+  // const objectProps = { hobby: "dance" };
   return (
     <>
       {/* <HelloWorld />
       <Greetings /> */}
-      <Props
+      {/* we can also pass array,object and numbers through props*/}
+      {/* <Props
         firstName="Avinash"
         lastName="Nemishte"
         age={22}
         arrayProps={arrayProps}
         objectProps={objectProps}
-      />
-      {/* we can also pass array,object and numbers through props*/}
+      /> */}
+      {/* <ChildrenProps>Hii there , How are you</ChildrenProps> */}
+      {/* we use map method to render list */}
+
+      {/* method 1
+      {users.map((user) => {
+        return (
+          <RenderList firstName={user.firstName} lastName={user.lastName} />
+        );
+      })} */}
+      {/* method 2 */}
+      {users.map((user) => (
+        <RenderList {...user} key={user.userId} />
+      ))}
     </>
   );
 }
---
+++ myapp/src/components/props/ChildrenProps.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChildrenProps({ children }) {
+  return (
+    <div>
+      <h1>{children}</h1>
+    </div>
+  );
+}
+
+export default ChildrenProps;
